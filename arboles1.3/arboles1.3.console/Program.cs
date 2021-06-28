f3062a55e4999b6f3433dac5a78a8f18f7fc1a7c
sebasmanu14 <sas.manobanda@yavirac.edu.ec>
2021-06-28 02:44:11 -0500

    deber arboles

---
+++ arboles1.3/arboles1.3.console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace arboles1._3.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
